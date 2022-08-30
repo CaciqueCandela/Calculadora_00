083c78ee83f3e1002bf3c5de820f0a556c9c2c76
Nelson Ramírez García <jaguar1077@gmail.com>
2022-08-29 20:53:00 -0500

    La calculadora se modifica totalmente para ampliar su capacidad, pero aún no es el momento de un nuevo desarrollo.

--- calculadoraFunciones_00.py
+++ calculadoraFunciones_00.py
@@ -14,91 +14,136 @@ Ejercicio: realizar una calculadora que permita las siguientes funciones:
 
 def run():
 
-    # Calcular suma:
+# Función para suma:
 
-    def suma(numero1, numero2):
-        return numero1 + numero2
+    from re import X
 
-    # Calcular resta:
 
-    def resta(numero1, numero2):
-        return numero1 - numero2
+    def suma():
+        numero1 = int(input('Por favor, ingrese el sumando uno: '))
+        numero2 = int(input('Por favor, ingrese el sumando dos: '))
+        print('La suma de ', numero1, '+', numero2, 'es', numero1+numero2)
 
-    # Calcular el multiplicación:
+# Función para resta:
 
-    def multiplicacion(numero1, numero2):
-        return numero1 * numero2
+    def resta():
+        numero1 = int(input('Por favor, ingrese el minuendo: '))
+        numero2 = int(input('Por favor, ingrese el sustraendo: '))
+        print('La diferencia de ', numero1, '-', numero2, 'es', numero1-numero2)
 
-    # Calcular división:
+# Función para multiplicación:
 
-    def division(numero1, numero2):
+    def multiplicacion():
+        numero1 = int(input('Por favor, ingrese el multiplicando: '))
+        numero2 = int(input('Por favor, ingrese el multiplicador: '))
+        print('El producto de ', numero1, '*', numero2, '=', numero1*numero2)
 
+# Función para división:
+
+    def division():
         try:
-            return numero1 / numero2
-
+            numero1 = int(input('Por favor, ingrese el dividendo: '))
+            numero2 = int(input('Por favor, ingrese el divisor: '))
+            print('El cociente de ', numero1, '/', numero2, '=', numero1/numero2)
         except ZeroDivisionError:
-            print('No se puede dividir entre 0')
-            return "Operación errónea."
+            print('ERROR: no se puede dividir por cero')
 
-    # Calcular el factorial:
+    
+# Calcular el factorial: 
+    '''Cantidad que resulta de la multiplicación de determinado número natural por
+    todos los números naturales que le anteceden excluyendo el cero; se representa por n!)'''
 
-    '''def factorial(numero1):
-        if numero1 < 0:
-            return None
+    def Factorial():
+        mi_factorial = int(input('\nPor favor, introduzca el número del que quiere calcular el factorial: '))
+        print('El factorial de', str(mi_factorial), 'es:', str(factorialCalculo(mi_factorial)))
 
-        if numero1 < 2:
+    def factorialCalculo(numero):
+        if numero <= 1: # No puede ser 0 porque cualquier multiplicación por 0 = 0.
             return 1
+        else:
+            # Es el resultado de multiplicar un número por todos los números anteriores.
+            return numero * factorialCalculo(numero - 1) 
 
-        return numero1 * factorial(numero1 - 1) # Esta es nuestra función recursiva.'''
+# Calcular la potencia:
+# Resultado de multiplicar un número por sí mismo una o varias veces.
 
-    # Calcular la potencia:
+    def Potencia():
+        base = int(input('Por favor, ingrese la base de la potencia: '))
+        exponente = int(input('Por favor, ingrese el exponente de la potencia: '))
+        print('\nEl valor de ' + str(base) + ' elevado a ' + str(exponente) + ' es: ' + str(potenciaCalculo(base, exponente)))
 
-    def Potencia(numero1, numero2):
-        if numero2 <= 0:
+    def potenciaCalculo(base, exponente):
+        if exponente <= 0:
             return 1
 
         else:
-            return numero1 * Potencia(numero1, numero2-1)
+            return base * potenciaCalculo(base, exponente-1)
 
+# Calcular la raíz cuadrada (Método babilónico):
+    '''
+    La raíz cuadrada es una operación matemática que se realiza sobre un número para conocer qué otro número,
+    multiplicado por sí mismo, da como resultado el número original.
+    '''
+
+    def raiz_cuadrada():
+        numero1 = int(input('Por favor, ingrese el radicando: '))
+        print('La raíz cuadrada de ', numero1, 'es', raiz_babilonica(numero1))
+    
+    def raiz_babilonica(numero1):
+        x = numero1 / 2
+
+        while True: # Necesitamos un bucle
+            if x * x == numero1:
+                return x # x
+            else:
+                copia_x = x
+                # Calculamos la media entre la base y la altura. "numero1" sería el área. "x" sería la base.
+                x = (x + (numero1/x)) / 2 
+            if copia_x == x:
+                break
+            # "x" que es la base del rectangulo se iría acercando a la base del rectangulo que buscamos.
+        return x
+
+
+# Segundo, se ingresa la función para la calculadora:
+
+    def Calculadora():
+        fin = False
+        while not(fin):
+            opcion = int(input('Por favor, escoge una opción y presiona Enter: '))
+            if (opcion == 1):
+                suma()
+            elif (opcion == 2):
+                resta()
+            elif (opcion == 3):
+                multiplicacion()
+            elif (opcion == 4):
+                division()
+            elif (opcion == 5):
+                Factorial()
+            elif (opcion == 6):
+                Potencia()
+            elif (opcion == 7):
+                raiz_cuadrada()  
+            elif (opcion == 8):
+                fin = 1
+            else:
+                print('\nOpción incorrecta.')
+                    
     # Ingresamos los valores:
 
-    print("\nCALCULADORA DE OPERACIONES BÁSICAS UTILIZANDO DOS VALORES\n")
-    
-    print("""
-    \t*Si la operación que desea realizar es una suma ingrese el simbolo +
-    \t*Si la operación que desea realizar es una resta ingrese el simbolo -
-    \t*Si la operación que desea realizar es una multiplicación ingrese el simbolo *
-    \t*Si la operación que desea realizar es una división ingrese el simbolo /
-    \t*Si la operación que desea realizar es una potencia ingrese el simbolo &
+    print('''\nCALCULADORA DE OPERACIONES BÁSICAS UTILIZANDO DOS VALORES BASADA EN FUNCIONES DE PYTHON
+        \nMenu:
+        1) Suma
+        2) Resta
+        3) Multiplicación
+        4) División
+        5) Factorial
+        6) Potencia
+        7) Raíz cuadrada
+        8) Salir\n''')
+    Calculadora()
 
-    """)
-    operation = input("\nIndique qué operación desea realizar y presione Enter: ")
-
-    numero1 = int(input("\nPor favor ingrese el primer valor y presione Enter: "))
-    numero2 = int(input("Por favor ingrese el segundo valor y presione Enter: "))
-    
-    # Ahora se deben ingresar las operaciones:
-
-    if operation == "+":
-        print('\nLa suma da como resultado: ', numero1 + numero2)
-
-    elif operation == "-":
-        print('\nLa diferencia da como resultado: ', numero1 - numero2)
-
-    elif operation == "*":
-        print('\nEl producto da como resultado: ', numero1 * numero2)
-
-    elif operation == "/":
-        print('\nEl cociente da como resultado: ', numero1 / numero2)
-
-        '''elif operation == "!":
-        print('\nEl número factorial de', (numero1), 'es', factorial(numero1) )'''
-
-    elif operation == "&":
-        print('\nEl valor de ' + str(numero1) + ' elevado a ' + str(numero2) + ' es: ' + str(Potencia(numero1, numero2)))
-
-    else:
-        print("\nOperación o simbolo incorrecto.")
 
 if __name__ == '__main__':
     run()
